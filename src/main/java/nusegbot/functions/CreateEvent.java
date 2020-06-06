04550d07d5b55b6e10bfbbad0d55e45e06d82e81
Chen Yifan <chen.yiafn@gmail.com>
2020-06-07 06:50:46 +0800

    fixed shit

--- src/main/java/nusegbot/functions/CreateEvent.java
+++ src/main/java/nusegbot/functions/CreateEvent.java
@@ -1,36 +1,20 @@
 package nusegbot.functions;
 
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 
-import nusegbot.DBConnection;
+//import nusegbot.DBConnection;
 
 public class CreateEvent {
 	
-	public static DBConnection db;
+	//public static DBConnection db;
+	public String event;
 	
-	public CreateEvent(DBConnection db2) {
-		db = db2;
+	public CreateEvent( String event) {
+
+		this.event = event;
 	}
-	public void selectTest() {
-//		db.connect();
-//		ResultSet rs = db.select("SELECT * FROM nusegbot.testtable;");
-//		try {
-//			String[] arr = null;
-//			while (rs.next()) {
-//			    String em = rs.getString("prikey");
-//			    arr = em.split("\n");
-//			    for (int i =0; i < arr.length; i++){
-//			        System.out.println(arr[i]);
-//			    }
-//			}
-//			db.close();
-//			System.out.println(arr[0]);
-//			return arr[0].toString();
-//		}
-//
-//		catch(Exception e) {
-//			System.out.println(e);
-//		}
-//		return "Not working";
+	public String formatEvent(String event) {
+		return event.substring(13);
 	}
 }
+
