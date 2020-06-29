c597269c85106450483e5fd3cdc44623c708f626
Chen Yifan <chen.yiafn@gmail.com>
2020-06-30 00:48:30 +0800

    Fixed compiled main class, added db check

--- src/main/java/nusegbot/Main.java
+++ src/main/java/nusegbot/Main.java
@@ -63,6 +63,21 @@ public class Main extends ListenerAdapter{
 				{
 					event.getChannel().sendMessage("Pong!").queue();
 				}
+				else if(msg.startsWith("/systemscheck") && (event.getAuthor().getId().equals("132420435294814208") || event.getAuthor().getId().equals("185836347536179200")))
+				{
+					event.getChannel().sendMessage("Command Succeeded...\nChecking Database...").queue();
+					try {
+						db.connect();
+						db.close();
+						Thread.sleep(2000);
+						event.getChannel().sendMessage("DB@35.240.223.254 is up...\nSystems looking green!").queue();
+					}
+					catch (Exception e)
+					{
+						event.getChannel().sendMessage("DB@35.240.223.254 is down!").queue();;
+					}
+					
+				}
 				else if (msg.startsWith("/help") && PermissionUtil.checkPermission(event.getMember(), Permission.ADMINISTRATOR))
 				{
 					event.getChannel().sendMessage(":hammer: Admin Help Panel :hammer:\n\n:exclamation: **General Information** :exclamation:\n1. Please remove all <> in commands and replace what is within it with the variables you want.\n\n\n:tada: **Giveaway Commands** :tada:\n1. /giveaway create <giveaway name> <pastebin raw url for igns> - Create a giveaway of that name and enters the igns of all participants into the giveaway via the url. [Please use this command in an admin channel.]\n\t - Select all the IGNs of participants of giveaway in Google Sheets/Excel and paste it into the New Paste section of https://pastebin.com. \n\t - Press the Create New Paste button. \n\t - Press the raw button.\n\t - Copy the URL of the page and use that URL in this command.\n2. /giveaway roll <giveaway name> - Rolls for a winner from the eligible list. The person rolled will no longer be eligible for this giveaway. (Can roll multiple times) [The command will be deleted. Please use this command where participants can see the roll happening.]\n3. /giveaway close <giveaway name> - Closes the giveaway forever (USE IT AT YOUR OWN RISK) [Please use this command in an admin channel].\n\n:mega: **Event Commands** :mega:\n1. /createevent <event name> | <event description> | <dates & time> | <games> | <signup link> - Create a new event announcement with the respective details. If a picture is required, upload the picture to discord first, then paste the command under 'Add a comment'. [Use this command under an event announcement channel where potential participants can view it. The original command will be deleted.]\n\n\n:person_tipping_hand: **Contact Us** :person_tipping_hand:\nIf you have any issues or require help, please contact: \ncira#2725 id:<132420435294814208> or \nCloey#7090 id:<185836347536179200>").queue();
