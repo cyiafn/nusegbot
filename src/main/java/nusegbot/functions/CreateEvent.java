f520f95eab083398b301bf8deb6d3a08ef5a2810
Cloey <cloeytwq@gmail.com>
2020-07-04 21:56:46 +0800

    Fixed and added createvent photos

--- src/main/java/nusegbot/functions/CreateEvent.java
+++ src/main/java/nusegbot/functions/CreateEvent.java
@@ -11,17 +11,24 @@ public class CreateEvent {
 	
 	public String formatEvent() {
 		
-		/* Format for command:
-
-		/createevent POST FINALS EVENT | We are having a post-finals event! | 23 July 8pm,9pm, 24 July 8pm,9pm | League of Legends, Overwatch | https://www.instagram.com/nusegaming/
-		
-		*/
+/* Format for command:
+IMG:
+/createevent POST FINALS EVENT | We are having a post-finals event! | 23 July 8pm,9pm, 24 July 8pm,9pm | League of Legends, Overwatch | https://www.instagram.com/nusegaming/ | https://i.imgur.com/Vs774wm.gif
+NO IMG:
+/createevent POST FINALS EVENT | We are having a post-finals event! | 23 July 8pm,9pm, 24 July 8pm,9pm | League of Legends, Overwatch | https://www.instagram.com/nusegaming/
+*/
 		
 		String parsed = event.substring(13);
 		
 		String[] parsedStr = parsed.split("\\|");
 		
-		System.out.println("0 " + parsedStr[0]);
+		Boolean img = false;
+		
+		System.out.println("arr size is " + parsedStr.length);
+		
+		if (parsedStr.length == 6) {
+			img = true;
+		}
 		
 		String waveEmoji = new String(Character.toChars(0x1F44B));
 		String clockEmoji = new String(Character.toChars(0x23F0));
@@ -30,24 +37,23 @@ public class CreateEvent {
 		String memoEmoji = new String(Character.toChars(0x1F4DD));
 		String heartEmoji = new String(Character.toChars(0x1F646));
 		String speechEmoji = new String(Character.toChars(0x1F4AC));
+		String picEmoji = new String(Character.toChars(0x1F4F8));
+		String fixEmoji = new String(Character.toChars(0x1F6E0));
 		
 		StringBuilder finalStr = new StringBuilder(" Hey @everyone! " + waveEmoji + "\n\n");
 		
-		/*
-		finalStr.append(megaphoneEmoji + " " + parsedStr[0] + megaphoneEmoji + "\n");
-		finalStr.append(speechEmoji + " Description: " + parsedStr[1] + "\n");
-		finalStr.append(clockEmoji + " Dates & Times: " + parsedStr[2] + "\n");
-		finalStr.append(gameEmoji + " Games: " + parsedStr[3] + "\n");
-		finalStr.append(memoEmoji +  " Sign-up Link: " + parsedStr[4] + "\n");
-		finalStr.append("See you there! " + heartEmoji);
-		*/
 		finalStr.append(megaphoneEmoji + " **" + parsedStr[0]  + "** " + megaphoneEmoji + "\n\n");
 		finalStr.append(speechEmoji + " " + parsedStr[1] + "\n");
 		finalStr.append(clockEmoji + " " + parsedStr[2] + "\n");
 		finalStr.append(gameEmoji + " " + parsedStr[3] + "\n");
-		finalStr.append(memoEmoji +  " " + parsedStr[4] + "\n\n");
-		finalStr.append("See you there! " + heartEmoji + "\n\n");
-		
+		finalStr.append(memoEmoji +  " " + parsedStr[4] + "\n");
+		if (img) {
+			finalStr.append(picEmoji + " " + parsedStr[5] + "\n");
+		}
+		finalStr.append("\nSee you there! " + heartEmoji + "\n\n");
+		if (img) {
+		finalStr.append(fixEmoji + " Not seeing images? Enable \"Display images when posted as links to chat\" under Settings > Text & Images!\n\n");
+		}
 		return finalStr.toString();
 	}
 }
